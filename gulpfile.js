553ea3dfa51778faaabaea458d02ac559295d81e
[John Patterson] <pattersonjc1@gmail.com>
2016-10-16 15:44:45 -0400

    tweaks to layout

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,26 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+var browserSync = require('browser-sync').create();
+
 gulp.task('sass', function () {
   return gulp.src('./sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({
+      stream: true
+    })
+    )});
+
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/*.scss', ['sass']);
+gulp.task('watch',['browserSync','sass'], function () {
+  gulp.watch(['./sass/*.scss','./*.html','./js/*.js'], ['sass']);
 });
--- main.js
+++ main.js
@@ -1,36 +1,49 @@
+jQuery(function() {
+
+    $("#button").on("click", function() {
+
+        var text = $("#seacrhBox").val();
+        getWeather(text);
+    })
+
+    function getWeather(query) {
+        $("#putWeatherHere").html("");
+
+        $.ajax({
+            url: `http://api.openweathermap.org/data/2.5/forecast/daily?q=${query},US&cnt=7&APPID=f5e364968f16eed20ecfaf7efa2d6303&units=imperial`,
+            method: 'GET',
+            success: function successHandler(weatherData) {
+                console.log(weatherData);
+
+                weatherData.list.forEach(function(weatherData) {
+                    $("#putWeatherHere").append(`
+                <tr>
+                        <td class="day_name">${moment.unix(weatherData.dt).format('dddd')}</td>
+                        <td class="weather_icon"><img src="http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png"></td>
+                        <td class="day_high">${weatherData.temp.max}</td>
+                        <td class="day_low">${weatherData.temp.min}</td>
+                </tr>
+                
+                                            `);
+
+                })
+            },
+        });
+    }
+
+});
 
 
 
-$("#button").on("click", function() {
 
-    var text = $("#seacrhBox").val();
-    getWeather(text);
-})
-
-function getWeather(query) {
-
-    $.ajax({
-        url: `http://api.openweathermap.org/data/2.5/forecast/daily?q=${query},US&cnt=7&APPID=f5e364968f16eed20ecfaf7efa2d6303&units=imperial`,
-        method: 'GET',
-        success: function successHandler(weatherData) {
-            console.log(weatherData);
-            $("#putWeatherHere").html("");
-            weatherData.list.forEach(function(weatherData) {
-                $("#putWeatherHere").append(`
-				
-				<tr class="day">
-						<td class="day_name">${moment.unix(weatherData.dt).format('dddd')}</td>
-
-          	<td class="weather_description">${weatherData.weather[0].description}</td> 
-						<td class="weather_icon"><img 					src="http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png"></td>
-             <td class="day_high">${weatherData.temp.max}</td>
-             <td class="day_low">${weatherData.temp.min}</td>
- 				</tr>
-
-
-							`);
-
-            })
-        },
-    });
-}
+// <table class="table">
+//                 <thead>
+//                 <tr>
+//                         <th>Day</th>
+//                         <th>Conditions</th>
+//                         <th>Conditions</th>
+//                         <th>High Temp</th>
+//                         <th>Low Temp</th>
+//                 </tr>
+//                 </thead>
+// </table>
